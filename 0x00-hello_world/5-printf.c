e5b0dde33d22332fafd09ec42e053bef5081ae9c
ajibad-coder <meriesbells1996@gmail.com>
2023-07-13 16:15:07 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include  <stdio.h>
+
+/**
+ *  main - excution program
+ *
+ *  Return: always zero
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
+
