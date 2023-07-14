f71b4d84bf7310efe2648c5483ddd340e250e9e6
ajibad-coder <meriesbells1996@gmail.com>
2023-07-14 15:19:07 -0700

    base 16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -excution programm
+ *
+ * Reason: always zero
+ */
+
+int main(void)
+{
+	int num;
+	char letter;
+
+	for (num = 0 ; num < 10 ; num++)
+		putchar(num + '0');
+	for (letter = 'a' ; letter <= 'f' ; letter++)
+		putchar(letter);
+	putchar('\n');
+	return (0);
+}
