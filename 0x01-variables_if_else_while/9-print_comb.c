ba795cc6e0aac8f380ac6ce1c6fb4f8e9483d27f
ajibad-coder <meriesbells1996@gmail.com>
2023-07-14 16:12:31 -0700

    commas fix

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,14 +11,13 @@ int main(void)
 
 	for (num = 0 ; num < 10 ; num++)
 	{
-		if (num == 9)
-			putchar(num + '0');
-		else
+		putchar(num + '0');
+		if (num != 9)
 		{
-			putchar(num + '0');
 			putchar(',');
 			putchar(' ');
 		}
 	}
+	putchar('\n');
 	return (0);
 }
