8dcdf347a2aec3c89f2407ef13964bf9a3d84b5a
ajibad-coder <meriesbells1996@gmail.com>
2023-07-14 14:34:08 -0700

    base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - excution programm
+ *
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0 ; num < 10 ; num++)
+		printf("%d", num);
+	printf("\n");
+	return (0);
+}
