17b5d5e2002367b82e7fe6dc4215ac589fade6be
ajibad-coder <meriesbells1996@gmail.com>
2023-07-18 14:07:37 -0700

    last number

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_last_digit - excution program func
+ *
+ * @last_num: arguement
+ *
+ * Return: num
+ */
+
+int print_last_digit(int last_num)
+{
+	int num = last_num % 10;
+
+	return (num);
+}
