6a24d3f8a57f2029747e803d25a7dfc2b9c3f449
ajibad-coder <meriesbells1996@gmail.com>
2023-07-18 15:49:21 -0700

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * add - excution program func
+ *
+ * @num: firts arguement
+ *
+ * @num2: second arguement
+ *
+ * Return: it will result answer
+ */
+
+
+int add(int num, int num2)
+{
+	int answer = num + num2;
+
+	return (answer);
+}
