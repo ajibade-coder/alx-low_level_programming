7d76ed1edfb883b6cf3d7270a83175cf05a83362
ajibad-coder <meriesbells1996@gmail.com>
2023-07-20 09:49:57 -0700

    multiply

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * mul - excution program for multiplication
+ *
+ * @a: first digit parameter
+ * @b: second digit parameter
+ *
+ * Return: answer
+ */
+
+
+
+
+int mul(int a, int b)
+{
+	int answer = a * b;
+
+	return (answer);
+}
