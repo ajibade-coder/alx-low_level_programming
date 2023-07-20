e3abe2a4579f2eb9a53030e8a6846b1bcd3c625d
ajibad-coder <meriesbells1996@gmail.com>
2023-07-20 00:55:19 -0700

    firt debugging

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/* betty style doc for function main goes there */
+/**
+ * positive_or_negative - excution program
+ *
+ * @n: arguement
+ *
+ * Return: always zero
+ */
+void positive_or_negative(int n)
+{
+
+	if (n > 0)
+		printf("%d is positive\n", n);
+	else if (n == 0)
+		printf("%d is zero\n", n);
+	else
+		printf("%d is negative\n", n);
+}
