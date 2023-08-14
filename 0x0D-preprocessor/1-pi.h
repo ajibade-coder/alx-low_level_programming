1376176d684cd612856ef93621508a8e86b1b221
ajibad-coder <meriesbells1996@gmail.com>
2023-08-14 14:14:00 -0700

    macro named PI

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,13 @@
+#ifndef pi_h
+#define pi_h
+
+/*
+ * Desc: Header file that defines a macro named PI
+ *       as an abbreviation for the token 3.14159265359.
+ */
+
+
+
+#define PI 3.14159265359
+
+#endif
