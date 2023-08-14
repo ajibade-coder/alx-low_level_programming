ba8ff86aa489a8c57bfef7e546e439f1495bbfca
ajibad-coder <meriesbells1996@gmail.com>
2023-08-14 15:18:48 -0700

    macro abs func

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,12 @@
+#ifndef function_like_macro_h
+#define function_like_macro_h
+
+/**
+ * ABS - Header computes the absolute value of a number x
+ * @x: number parameter
+ */
+
+
+#define ABS(x) (x < 0 ? x * -1 : x)
+
+#endif
