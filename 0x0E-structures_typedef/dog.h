e30b7ab9d8a9a2faabf9e3506660fb52d343db50
ajibad-coder <meriesbells1996@gmail.com>
2023-08-14 09:12:22 -0700

    struct dog

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef dog_h
+#define dog_h
+/**
+ * struct dog - dog entity
+ * @name: dog name value holder
+ * @age: dog age value holder
+ * @owner: dog owner name value holder
+ *
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
+
