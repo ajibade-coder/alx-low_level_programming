6fe86b7636f0f4efcfba72573209e70b4f11db0a
ajibad-coder <meriesbells1996@gmail.com>
2023-08-14 14:05:29 -0700

    macro named SIZE

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,13 @@
+#ifndef object_like_macro_h
+#define object_like_macro_h
+
+/*
+ * File: 0-object_like_macro.h
+ * Auth: Brennan D Baraban
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
+
+#define SIZE 1024
+
+#endif
